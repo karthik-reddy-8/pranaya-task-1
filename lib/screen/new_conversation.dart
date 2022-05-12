ded300140588d122d9bea5bc5d65bedf1307dba0
pranay <pranayab.in@mouritech.com>
2022-05-12 11:31:02 +0530

    final commit

--- lib/screen/new_conversation.dart
+++ lib/screen/new_conversation.dart
@@ -76,7 +76,8 @@ class _ConversationState extends State<Conversation> {
                     Icons.supervisor_account_rounded,
                     color: Colors.lightBlueAccent,
                   ),
-                  SizedBox(
+                  SizedBox
+
                     width: 5,
                   ),
                   Text(
